4c2c3d336d908180ed4208208e66256014352dbd
Patricio Perez <62504leo@universidaddeleon.edu.mx>
2025-10-06 08:56:35 -0600

    se agrego area de circulo

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
-from clases.areas import Triangulo, Rectangulo
+from clases.areas import Triangulo, Rectangulo, Circulo
 
 def main():
     print("Cálculo de áreas de figuras geométricas")
     print("1. Triángulo")
     print("2. Rectángulo")
+    print("3. Círculo")
 
-    opcion = input("Elija una opción (1 o 2): ")
+    opcion = input("Elija una opción (1, 2 o 3): ")
 
     if opcion == "1":
         figura = Triangulo()
     elif opcion == "2":
         figura = Rectangulo()
+    elif opcion == "3":
+        figura = Circulo()
     else:
         print("Opción no válida.")
         return
@@ -22,3 +25,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
